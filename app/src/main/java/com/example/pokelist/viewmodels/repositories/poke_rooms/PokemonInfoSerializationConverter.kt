f1a640c11fd0a48481454d340ac57ffcf9bc0bc6
James Dave Navor <esteban.dave999@gmail.com>
2023-08-30 10:54:03 +0800

    Optimized TypeConverter    Implemented Single Function Responsibility for every Type Converter

--- app/src/main/java/com/example/pokelist/viewmodels/repositories/poke_rooms/PokemonInfoSerializationConverter.kt
+++ app/src/main/java/com/example/pokelist/viewmodels/repositories/poke_rooms/PokemonInfoSerializationConverter.kt
@@ -6,77 +6,53 @@ import com.example.pokelist.viewmodels.repositories.poke_api.entities.AbilityInf
 import com.example.pokelist.viewmodels.repositories.poke_api.entities.MoveInfo
 import com.example.pokelist.viewmodels.repositories.poke_api.entities.Moves
 import com.example.pokelist.viewmodels.repositories.poke_api.entities.Sprites
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class PokemonInfoSerializationConverter {
     @TypeConverter
-    fun abilitiesListToString(list: List<Ability>): String{
-        return Json.encodeToString(list)
-    }
+    fun abilitiesListToString(list: List<Ability>): String = list.encode()
 
     @TypeConverter
-    fun stringToAbilitiesList(data: String): List<Ability>{
-        return Json.decodeFromString(data)
-    }
+    fun stringToAbilitiesList(data: String): List<Ability> = data.decode()
 
     @TypeConverter
-    fun abilitiesToString(ability: Ability): String{
-        return Json.encodeToString(ability)
-    }
+    fun abilitiesToString(ability: Ability): String = ability.encode()
 
     @TypeConverter
-    fun stringToAbilities(data: String): Ability{
-        return Json.decodeFromString(data)
-    }
+    fun stringToAbilities(data: String): Ability = data.decode()
 
     @TypeConverter
-    fun abilityInfoToString(info: AbilityInfo): String{
-        return Json.encodeToString(info)
-    }
+    fun abilityInfoToString(info: AbilityInfo): String = info.encode()
 
     @TypeConverter
-    fun stringToAbilityInfo(data: String): AbilityInfo{
-        return Json.decodeFromString(data)
-    }
+    fun stringToAbilityInfo(data: String): AbilityInfo = data.decode()
 
     @TypeConverter
-    fun movesListToString(list: List<Moves>): String{
-        return Json.encodeToString(list)
-    }
+    fun movesListToString(list: List<Moves>): String = list.encode()
 
     @TypeConverter
-    fun stringToMovesList(data: String): List<Moves>{
-        return Json.decodeFromString(data)
-    }
+    fun stringToMovesList(data: String): List<Moves> = data.decode()
 
     @TypeConverter
-    fun movesToString(moves: Moves): String{
-        return Json.encodeToString(moves)
-    }
+    fun movesToString(moves: Moves): String = moves.encode()
 
     @TypeConverter
-    fun stringToMoves(data: String): Moves{
-        return Json.decodeFromString(data)
-    }
+    fun stringToMoves(data: String): Moves = data.decode()
 
     @TypeConverter
-    fun moveInfoToString(move: MoveInfo): String{
-        return Json.encodeToString(move)
-    }
+    fun moveInfoToString(move: MoveInfo): String = move.encode()
 
     @TypeConverter
-    fun stringToMoveInfo(data: String): MoveInfo{
-        return Json.decodeFromString(data)
-    }
+    fun stringToMoveInfo(data: String): MoveInfo = data.decode()
 
     @TypeConverter
-    fun spritesToString(sprites: Sprites): String{
-        return Json.encodeToString(sprites)
-    }
+    fun spritesToString(sprites: Sprites): String = sprites.encode()
 
     @TypeConverter
-    fun stringToSprites(data: String): Sprites {
-        return Json.decodeFromString(data)
-    }
+    fun stringToSprites(data: String): Sprites = data.decode()
+
+    private fun Any.encode(): String = Json.encodeToString(this)
+    private inline fun <reified T: Any> String.decode(): T = Json.decodeFromString(this)
 }
