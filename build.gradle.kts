fa9187debed828cceba60f34ac0f25b89ec9c2c3
James Dave Navor <james.navor@abedatasolutions.com.ph>
2026-03-14 17:42:30 +0800

    Reconfigured Gradle Build System:    Upgraded Gradle to 9.3.1    Upgraded Android Gradle Plugin to 9.1.0    Implemented Foojay Toolchain Resolver for JVM 17    Reconfigured Kotlin and KSP dependencies for AGP 9.0 compatibility    Added Compose Multiplatform and KMP plugin definitions to Version Catalog    Removed deprecated kotlinAndroid plugin references

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        // Upgrade to a higher KGP version instead of built-in Kotlin
+        // https://developer.android.com/build/releases/agp-9-0-0-release-notes?utm_source=android-studio-app&utm_medium=app#runtime-dependency-on-kotlin-gradle-plugin-upgrade
+        classpath(libs.kotlinGradlePlugin)
+        classpath(libs.kspGradlePlugin)
+    }
+}
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinPluginSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinParcelize) apply false
