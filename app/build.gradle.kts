b3b0c60e94da200fd0e94b9940c513f8957d7731
James Dave Navor <james.navor@abedatasolutions.com.ph>
2026-03-15 11:39:53 +0800

    Updated Java Toolchain to 21:    Updated Source and Target Compatibility    Updated Kotlin JVM Target    Updated Gradle Daemon JVM Version and Toolchain URLs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         viewBinding = true
@@ -37,7 +37,7 @@ android {
 }
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
