2441ebbf26d5157084c569d0e58f83a3f1d66394
James Dave Navor <james.navor@abedatasolutions.com.ph>
2026-03-15 13:46:32 +0800

    Refactored BaseConventionPlugin    Removed nullability from target Project in apply method

--- buildLogic/convention/src/main/kotlin/BaseConventionPlugin.kt
+++ buildLogic/convention/src/main/kotlin/BaseConventionPlugin.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class BaseConventionPlugin: Plugin<Project> {
-    override fun apply(target: Project?) {}
+    override fun apply(target: Project) {}
 
     companion object {
         const val PLUGIN_ID = "project.base"
